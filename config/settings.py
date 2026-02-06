b03c25506847e7931e73ac7d15e51ac82f1759b0
Usuario <wbm.andres@gmail.com>
2026-02-06 15:08:58 -0500

    Remover dependencia de .env en settings.py

--- config/settings.py
+++ config/settings.py
@@ -12,15 +12,11 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
-from dotenv import load_dotenv
-
-# Cargar variables de entorno desde .env
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Variables de entorno
+# Cargar variables de entorno
 DATABASE_URL = os.environ.get('DATABASE_URL', None)
 
 
